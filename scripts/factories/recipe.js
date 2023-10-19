164db0c9c664aa373ca761485808c420046c9739
Ben Bld <bld.prom@gmail.com>
2023-10-19 17:20:37 +0200

    fix indentation and var names

--- scripts/factories/recipe.js
+++ scripts/factories/recipe.js
@@ -1,47 +1,47 @@
 export default class recipeTemplate {
     constructor(data) {
-    const { id, image, name, ingredients, time, description } = data;
-    this.id = id;
-    this.image = image;
-    this.name = name;
-    this.ingredients = ingredients;
-    this.time = time;
-    this.description = description
-    this.picture = image ? `./assets/images/${image}` : null;
+        const { id, image, name, ingredients, time, description } = data;
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.time = time;
+        this.description = description
+        this.picture = image ? `./assets/images/${image}` : null;
     }
 
     getRecipeCardDOM() {
         //recipe section
         const recipesSection = document.getElementsByClassName("container-recipes")[0];
 
-        const divRecipe = document.createElement('article');
-        divRecipe.className = "recipe-card";
-        divRecipe.id = this.id;
+        const containerRecipe = document.createElement('article');
+        containerRecipe.className = "recipe-card";
+        containerRecipe.id = this.id;
         //time 
         const timeRecipe = document.createElement('span');
         timeRecipe.className = "recipe-card__time";
-        timeRecipe.textContent = this.time+"min";
+        timeRecipe.textContent = this.time + "min";
         //image
         const imgRecipe = document.createElement('img');
         imgRecipe.className = "recipe-card__image";
         imgRecipe.setAttribute("src", this.picture);
         imgRecipe.setAttribute("alt", this.name);
         //content
-        const divContentRecipe = document.createElement('div');
-        divContentRecipe.className = "recipe-card__content";
+        const contentCardRecipe = document.createElement('div');
+        contentCardRecipe.className = "recipe-card__content";
         const titleRecipe = document.createElement("h2");
         titleRecipe.textContent = this.name;
 
         const titleDescriptionRecipe = document.createElement("h3");
         titleDescriptionRecipe.textContent = "Recette";
-        const divDescriptionRecipe = document.createElement('div');
-        divDescriptionRecipe.className = "recipe-card__content__description";
-        divDescriptionRecipe.textContent = this.description;
+        const contentDescriptionRecipe = document.createElement('div');
+        contentDescriptionRecipe.className = "recipe-card__content__description";
+        contentDescriptionRecipe.textContent = this.description;
 
         const titleIngredientsRecipe = document.createElement('h3');
         titleIngredientsRecipe.textContent = "Ingrédients";
-        const divIngredientsRecipe = document.createElement('div');
-        divIngredientsRecipe.className = "recipe-card__content__ingredients";
+        const contentIngredientsRecipe = document.createElement('div');
+        contentIngredientsRecipe.className = "recipe-card__content__ingredients";
 
         let listIngredientsRecipe = this.ingredients;
         for (let ingredient of listIngredientsRecipe) {
@@ -61,20 +61,20 @@ export default class recipeTemplate {
                 quantityIngredient.textContent += ingredient["unit"];
             }
 
-            divIngredientsRecipe.appendChild(divIngredient);
+            contentIngredientsRecipe.appendChild(divIngredient);
             divIngredient.appendChild(titleIngredient);
             divIngredient.appendChild(quantityIngredient);
         }
 
-        recipesSection.appendChild(divRecipe);
-        divRecipe.appendChild(timeRecipe);
-        divRecipe.appendChild(imgRecipe);
-        divRecipe.appendChild(divContentRecipe);
-        divContentRecipe.appendChild(titleRecipe);
-        divContentRecipe.appendChild(titleDescriptionRecipe);
-        divContentRecipe.appendChild(divDescriptionRecipe);
-        divContentRecipe.appendChild(titleIngredientsRecipe);
-        divContentRecipe.appendChild(divIngredientsRecipe);
+        recipesSection.appendChild(containerRecipe);
+        containerRecipe.appendChild(timeRecipe);
+        containerRecipe.appendChild(imgRecipe);
+        containerRecipe.appendChild(contentCardRecipe);
+        contentCardRecipe.appendChild(titleRecipe);
+        contentCardRecipe.appendChild(titleDescriptionRecipe);
+        contentCardRecipe.appendChild(contentDescriptionRecipe);
+        contentCardRecipe.appendChild(titleIngredientsRecipe);
+        contentCardRecipe.appendChild(contentIngredientsRecipe);
 
         return (recipesSection);
     }
